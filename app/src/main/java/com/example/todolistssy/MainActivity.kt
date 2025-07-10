68b89354c8bdd8cd395b2c5147a30f18d0db6018
stevey-sy <sinsy89@hanmail.net>
2025-07-10 23:46:29 +0900

    MainActivity 수정

--- app/src/main/java/com/example/todolistssy/MainActivity.kt
+++ app/src/main/java/com/example/todolistssy/MainActivity.kt
@@ -8,8 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.todolistssy.presentation.home.HomeScreen
@@ -20,34 +25,41 @@ import androidx.hilt.navigation.compose.hiltViewModel
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TodolistssyTheme {
                 val homeViewModel: HomeViewModel = hiltViewModel()
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = stringResource(id = R.string.app_title))
+                            },
+                            actions = {
+                                IconButton(
+                                    onClick = {
+                                        // TODO: History 화면으로 이동 또는 기능 추가
+                                    }
+                                ) {
+                                    Icon(
+                                        imageVector = AppIcons.History,
+                                        contentDescription = "History"
+                                    )
+                                }
+                            }
+                        )
+                    }
+                ) { innerPadding ->
                     HomeScreen(
-                        viewModel = homeViewModel
+                        viewModel = homeViewModel,
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodolistssyTheme {
-        Greeting("Android")
     }
 }
