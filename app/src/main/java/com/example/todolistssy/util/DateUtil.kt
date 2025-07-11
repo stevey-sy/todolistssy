7b57bc1d759ec182a61859fcbf6203da9b6a607f
stevey-sy <sinsy89@hanmail.net>
2025-07-11 11:53:55 +0900

    DateUtil -> time 메서드 추가

--- app/src/main/java/com/example/todolistssy/util/DateUtil.kt
+++ app/src/main/java/com/example/todolistssy/util/DateUtil.kt
@@ -6,12 +6,21 @@ import java.util.Locale
 
 object DateUtil {
     private val dateFormat = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault())
+    private val dateTimeFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.getDefault())
     
     fun formatDate(timestamp: Long): String {
         return dateFormat.format(Date(timestamp))
     }
     
+    fun formatDateTime(timestamp: Long): String {
+        return dateTimeFormat.format(Date(timestamp))
+    }
+    
     fun formatDateFromDate(date: Date): String {
         return dateFormat.format(date)
+    }
+    
+    fun formatDateTimeFromDate(date: Date): String {
+        return dateTimeFormat.format(date)
     }
 } 
