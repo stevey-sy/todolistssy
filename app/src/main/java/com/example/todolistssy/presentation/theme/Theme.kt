55b9939d7c435611cf3d904992e467be495a5153
stevey-sy <sinsy89@hanmail.net>
2025-07-11 12:49:02 +0900

    Theme 소스 정리

--- app/src/main/java/com/example/todolistssy/presentation/theme/Theme.kt
+++ app/src/main/java/com/example/todolistssy/presentation/theme/Theme.kt
@@ -11,15 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = TodoColors.NormalGray,
+    secondary = TodoColors.StealGray,
+    tertiary = TodoColors.Orange
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = TodoColors.NormalGray,
+    secondary = TodoColors.StealGray,
+    tertiary = TodoColors.Orange
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
